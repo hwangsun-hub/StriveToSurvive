0784ee1c0f2ed04dd82086d55cbb272e656178f7
hwangsun-hub <hwangsun0512@gmail.com>
2023-10-27 13:14:51 +0900

    player class update

--- StriveToSurvive/library.h
+++ StriveToSurvive/library.h
@@ -7,6 +7,8 @@
 #include <raylib.h>
 #include <vector>
 #include <string>
+#include <math.h>
+#include <raymath.h>
 
 //Global Constants
 constexpr int window_start_width = 1280;
@@ -17,14 +19,18 @@ constexpr char window_name[]{ "Strive To Survive" };
 //player.cpp
 class Player{
 public:
-	void Move() {};
-	void Shoot() {};
-	void Dodge() {};
+	//
+	void Move(bool, bool, bool, bool) {};
+	void Shoot(bool) {};
+	void Dodge(bool) {};
+	void Skill(bool) {};
+
+	Vector2 GetPosition() {};
 
 private:
 	enum BodyState{
 		PLAYER_STANDING,
-		PLAYER_WALKING,
+		PLAYER_WALKING
 	};
 
 	enum ArmState {
@@ -38,7 +44,18 @@ private:
 	};
 
 	Vector2 position{};
-	int hp;
+
+	int hp = 100;
+	float damage = 10;
+	float speed = 10;
+
+	//By default in seconds
+	float shooting_cooltime = 1;
+	float skill_cooltime = 10;
+	
+	//By default looking right
+	bool islookingleft = false;
+	bool islookingright = true;
 
 };
 
