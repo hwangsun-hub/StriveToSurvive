2ccdefab15fd90cb8abb5f83edbf26d9fd8833f5
hwangsun-hub <hwangsun0512@gmail.com>
2023-10-31 11:47:58 +0900

    Enemy update

--- StriveToSurvive/Enemy.cpp
+++ StriveToSurvive/Enemy.cpp
@@ -1,1 +1,10 @@
 #include "library.h"
+
+Enemy::Enemy(Player* player) : player(player) {
+	position = player->GetPosition();
+}
+
+
+void Enemy::UpdateSpawnPoint() {
+
+}
--- StriveToSurvive/library.h
+++ StriveToSurvive/library.h
@@ -15,6 +15,7 @@
 constexpr int WINDOW_START_WIDTH = 1280;
 constexpr int WINDOW_START_HEIGHT = 720;
 constexpr int WINDOW_FRAMES_PER_SECOND = 60;
+constexpr float SPAWNPOINT_CIRCLE_RADIUS = 750;	//{sqrt(WINDOW_START_WIDTH ^ 2 + WINDOW_START_HEIGHT ^ 2) / 2}'s approximation
 constexpr char WINDOW_NAME[]{ "Strive To Survive" };
 
 enum WeaponMeleeType {
@@ -75,14 +76,24 @@ public:
 //enemy.cpp
 class Enemy {
 private:
-	Player player;
+	Player *player;
 	Vector2 position;
-	Vector2 spawnpoint[6];
+	Vector2 spawnpoint[8] = {
+		{0, SPAWNPOINT_CIRCLE_RADIUS},
+		{SPAWNPOINT_CIRCLE_RADIUS / sqrt(2), SPAWNPOINT_CIRCLE_RADIUS / sqrt(2)},
+		{SPAWNPOINT_CIRCLE_RADIUS, 0},
+		{SPAWNPOINT_CIRCLE_RADIUS / sqrt(2), -SPAWNPOINT_CIRCLE_RADIUS / sqrt(2)},
+		{0, SPAWNPOINT_CIRCLE_RADIUS},
+		{-SPAWNPOINT_CIRCLE_RADIUS / sqrt(2), -SPAWNPOINT_CIRCLE_RADIUS / sqrt(2)},
+		{-SPAWNPOINT_CIRCLE_RADIUS, 0},
+		{-SPAWNPOINT_CIRCLE_RADIUS / sqrt(2), SPAWNPOINT_CIRCLE_RADIUS / sqrt(2)}
+	};
 protected:
 	bool isPlayerFollowType = true;
 public: 
-	Enemy(Player player);
-	
+	Enemy(Player* player);
+	void UpdateSpawnPoint();
+
 };
 
 
